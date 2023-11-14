7733e6ecf1981f182f1066b934148fb391ded022
Rojotiana <rojotiana.tsiverisoa@gmail.com>
2023-11-14 18:02:24 +0300

    add data

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:movie_app/colors/my_colors.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,10 +12,231 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    var size = MediaQuery.of(context).size;
+
+    return SafeArea(
       child: Scaffold(
+        backgroundColor: MyColors.c2,
         body: SingleChildScrollView(
-          child: Text('Hello'),
+          child: Column(
+            children: [
+              Container(
+                color: MyColors.c1,
+                child: Image(
+                  image: const AssetImage(
+                    'assets/images/logo.png',
+                  ),
+                  fit: BoxFit.contain,
+                  width: size.width,
+                  height: 60,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  bottom: 15,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: SizedBox(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //  ----------- Actions -----------
+                      const Text(
+                        'Actions',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: MyColors.c1,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        width: size.width,
+                        height: size.height,
+                        child: GridView(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 10,
+                          ),
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                // backgroundColor: MyColors.c1,
+                                padding: const EdgeInsets.all(0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: () => {},
+                              child: SizedBox(
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: const BorderRadius.all(
+                                        Radius.circular(20),
+                                      ),
+                                      child: Image.asset(
+                                        'assets/images/actions/img_1.jpg',
+                                        fit: BoxFit.cover,
+                                        width: size.width,
+                                        height: size.height,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        padding: const EdgeInsets.all(16),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(0.5),
+                                          borderRadius: const BorderRadius.only(
+                                            bottomLeft: Radius.circular(20),
+                                            bottomRight: Radius.circular(20),
+                                          ),
+                                        ),
+                                        child: const Text(
+                                          'The Creator (2023)',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                // backgroundColor: MyColors.c1,
+                                padding: const EdgeInsets.all(0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: () => {},
+                              child: SizedBox(
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: const BorderRadius.all(
+                                        Radius.circular(20),
+                                      ),
+                                      child: Image.asset(
+                                        'assets/images/actions/img_2.jpg',
+                                        fit: BoxFit.cover,
+                                        width: size.width,
+                                        height: size.height,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        padding: const EdgeInsets.all(16),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(0.5),
+                                          borderRadius: const BorderRadius.only(
+                                            bottomLeft: Radius.circular(20),
+                                            bottomRight: Radius.circular(20),
+                                          ),
+                                        ),
+                                        child: const Text(
+                                          'Darkland Undercover (2023)',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                // backgroundColor: MyColors.c1,
+                                padding: const EdgeInsets.all(0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: () => {},
+                              child: SizedBox(
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: const BorderRadius.all(
+                                        Radius.circular(20),
+                                      ),
+                                      child: Image.asset(
+                                        'assets/images/actions/img_3.jpg',
+                                        fit: BoxFit.cover,
+                                        width: size.width,
+                                        height: size.height,
+                                      ),
+                                    ),
+                                    Positioned(
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        padding: const EdgeInsets.all(16),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black.withOpacity(0.5),
+                                          borderRadius: const BorderRadius.only(
+                                            bottomLeft: Radius.circular(20),
+                                            bottomRight: Radius.circular(20),
+                                          ),
+                                        ),
+                                        child: const Text(
+                                          'Becky 2 : The Wrath of Becky (2023)',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: MyColors.c1,
+                                padding: const EdgeInsets.all(0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              onPressed: () => {},
+                              child: const SizedBox(
+                                child: Text(
+                                  'Voir Plus',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
