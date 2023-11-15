2d0e83d10f6cf9eae7c537951e6482ec3848c339
Rojotiana <rojotiana.tsiverisoa@gmail.com>
2023-11-15 10:26:20 +0300

    create two screen

---
+++ lib/screen/movie_lists.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoviesList extends StatefulWidget {
+  const MoviesList({super.key});
+
+  @override
+  State<MoviesList> createState() => _MoviesListState();
+}
+
+class _MoviesListState extends State<MoviesList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
